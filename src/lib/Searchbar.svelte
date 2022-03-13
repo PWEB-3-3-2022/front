9e58fb63f20009ac92cb38e51b4211f5e927b173
Badot <bastien.badot@gmail.com>
2022-03-13 17:44:36 +0100

    SearchBar and search icon

---
+++ src/lib/Searchbar.svelte
@@ -0,0 +1,21 @@
+<div class="searchContainer">
+	<input type="text" id="search-input" placeholder="Search..">
+</div>
+
+<style>
+	.searchContainer{
+		display: block;
+		float: left;
+		padding-left: 33%;
+	}
+	#search-input{
+		background-image: url("https://img.icons8.com/ios-glyphs/30/000000/search--v1.png");
+    	background-repeat: no-repeat;
+		text-decoration: none;
+		padding-left: 30px;
+		width: 500px;
+		font-size: 17px;
+		color: black;
+		text-align: left;
+	}
+</style>
