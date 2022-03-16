9271124e652baeb4d3bdc37346cf69ae924fd817
Guillaume Anthouard <guillaume.anthouard@hotmail.fr>
2022-03-16 16:54:55 +0100

    Add search results page

--- src/lib/Searchbar.svelte
+++ src/lib/Searchbar.svelte
@@ -1,21 +1,36 @@
+<script>
+
+  import { push } from 'svelte-spa-router';
+
+  let query = '';
+
+  function onSearch(e) {
+    e.preventDefault();
+    push(`/search?query=${query}`);
+  }
+</script>
+
 <div class="searchContainer">
-	<input type="text" id="search-input" placeholder="Search..">
+    <form on:submit={onSearch}>
+        <input type="text" id="search-input" bind:value={query} name="query" placeholder="Search...">
+    </form>
 </div>
 
 <style>
-	.searchContainer{
-		display: block;
-		float: left;
-		padding-left: 33%;
-	}
-	#search-input{
-		background-image: url("https://img.icons8.com/ios-glyphs/30/000000/search--v1.png");
-    	background-repeat: no-repeat;
-		text-decoration: none;
-		padding-left: 30px;
-		width: 500px;
-		font-size: 17px;
-		color: black;
-		text-align: left;
-	}
+    .searchContainer {
+        display: block;
+        float: left;
+        padding-left: 33%;
+    }
+
+    #search-input {
+        background-image: url("https://img.icons8.com/ios-glyphs/30/000000/search--v1.png");
+        background-repeat: no-repeat;
+        text-decoration: none;
+        padding-left: 30px;
+        width: 500px;
+        font-size: 17px;
+        color: black;
+        text-align: left;
+    }
 </style>
---
+++ src/pages/Search.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { querystring } from 'svelte-spa-router';
+  import Mosaic from '../lib/Mosaic.svelte';
+
+  // Parse query string parameters everytime they change
+  $: params = new URLSearchParams($querystring);
+
+  const items = [];
+  for (let i = 0; i < 4; i += 1) {
+    items.push('https://api.lorem.space/image/movie?w=180&h=220');
+  }
+</script>
+
+<main>
+    <span>Results for "{params.get('query')}"</span>
+    <Mosaic {items}/>
+</main>
