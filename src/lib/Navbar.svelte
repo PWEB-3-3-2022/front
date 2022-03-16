d559f8feefd98650476588f925bcc2fd875145ab
Lufou <lucas.damase@laposte.net>
2022-03-16 17:19:20 +0100

    Initial login page

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -2,6 +2,7 @@
   import Logo from './Logo.svelte';
   import Hamburger from './Hamburger.svelte';
   import Searchbar from './Searchbar.svelte';
+  import Login_button from './Login_button.svelte';
 
   export let sidebar = false;
 </script>
@@ -11,6 +12,15 @@
         <Hamburger bind:open={sidebar}/>
         <Logo/>
         <Searchbar/>
+        <div class="login">
+           <Login_button/>
+        </div>
     </nav>
-
 </header>
+
+<style>
+  .login {
+    position: absolute;
+    right: 100px;
+  }
+</style>
