0eae6bc3a6410da66d216c7dbf949789d1642107
Badot <bastien.badot@gmail.com>
2022-03-18 18:13:46 +0100

    Removed the suggestion now directly update result page on key pressed

--- src/lib/Searchbar.svelte
+++ src/lib/Searchbar.svelte
@@ -1,6 +1,5 @@
 <script>
   import { push } from 'svelte-spa-router';
-  import { searchMedias } from '../api';
 
   let query = '';
 
@@ -10,43 +9,15 @@
   }
 
   function onKeyUp() {
-    const autocomWrapper = document.querySelector('#autocom-box');
-    if (`${query}` !== '') {
-      searchMedias(`${query}`)
-        .then((result) => (function UpdateAutocom() {
-          autocomWrapper.innerHTML = '';
-          if (result.length !== 0) {
-            autocomWrapper.style.display = 'block';
-            result.forEach((element) => {
-              const newLi = document.createElement('li');
-              newLi.onclick = function redirect() {
-                push(`/search?query=${element.title}`);
-              };
-              newLi.id = 'autocom-li';
-              const newContent = document.createTextNode(element.title);
-              newLi.appendChild(newContent);
-              autocomWrapper.appendChild(newLi);
-            });
-          } else {
-            autocomWrapper.style.display = 'none';
-          }
-        }()))
-        .catch((error) => console.error(error));
-    } else {
-      autocomWrapper.style.display = 'none';
-    }
+    push(`/search?query=${query}`);
   }
 </script>
 
 <div class="searchContainer">
     <form on:submit={onSearch}>
         <input type="text" id="search-input" bind:value={query} on:keyup={onKeyUp} name="query" placeholder="Search...">
-        <div id="autocom-box">
-            <!-- here list are inserted from javascript -->
-        </div>
     </form>
 </div>
-
 
 <style>
     .searchContainer {
@@ -64,25 +35,5 @@
         font-size: 17px;
         color: black;
         text-align: left;
-    }
-
-    #autocom-box{
-        position: absolute;
-        width: 500px;
-        opacity: 1;
-        max-height: 280px;
-        overflow-y: auto;
-        background: white;
-    }
-
-    :global(#autocom-li){
-        color: black;
-        font-size: 17px;
-        padding-left: 30px;
-        width: 100%;
-    }
-
-    :global(#autocom-li:hover){
-        background: #efefef;   
     }
 </style>
