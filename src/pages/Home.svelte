9c46fa1ea95ffade350b7b67935b84a9a1ae2379
ndelprat <nicolas.delprat@insa-lyon.fr>
2022-03-26 12:29:31 +0100

    Popup component

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Modal from 'svelte-simple-modal';
   import Mosaic from '../lib/Mosaic.svelte';
   import { homeMovies, homeTvshows } from '../api.js';
 
@@ -7,38 +8,40 @@
 </script>
 
 <main>
-    <section>
-        <h1 class="section-header">Movies</h1>
-        {#await movies}
-            <p>Loading ...</p>
-        {:then movies}
-            {#await movies.json()}
-                <p>Loading</p>
+    <Modal>
+        <section>
+            <h1 class="section-header">Movies</h1>
+            {#await movies}
+                <p>Loading ...</p>
             {:then movies}
-                <Mosaic medias={movies}/>
+                {#await movies.json()}
+                    <p>Loading</p>
+                {:then movies}
+                    <Mosaic medias={movies}/>
+                {:catch err}
+                    <p>Error : {err}</p>
+                {/await}
             {:catch err}
                 <p>Error : {err}</p>
             {/await}
-        {:catch err}
-            <p>Error : {err}</p>
-        {/await}
-    </section>
-    <section>
-        <h1 class="section-header">TV Shows</h1>
-        {#await tvshows}
-            <p>Loading ...</p>
-        {:then tvshows}
-            {#await tvshows.json()}
-                <p>Loading</p>
+        </section>
+        <section>
+            <h1 class="section-header">TV Shows</h1>
+            {#await tvshows}
+                <p>Loading ...</p>
             {:then tvshows}
-                <Mosaic medias={tvshows}/>
+                {#await tvshows.json()}
+                    <p>Loading</p>
+                {:then tvshows}
+                    <Mosaic medias={tvshows}/>
+                {:catch err}
+                    <p>Error : {err}</p>
+                {/await}
             {:catch err}
                 <p>Error : {err}</p>
             {/await}
-        {:catch err}
-            <p>Error : {err}</p>
-        {/await}
-    </section>
+        </section>
+    </Modal>
 </main>
 
 <style>
